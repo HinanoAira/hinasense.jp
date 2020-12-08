a5701e47f74f3367e0567db6ef923b9d762a424e
雛野綾桜 <islakioriy@gmail.com>
2020-12-08 19:42:17 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     occupation: `ソフトウェアエンジニアを目指して`,
     keywords: [`雛野`, `綾桜`, `Personal`, `Blog`, `Resume`, `Projects`, `Work`],
     siteUrl:
-      `http://www.hinasense.jp:8000`,
+      process.env.URL || process.env.DEPLOY_URL || `http://www.hinasense.jp`,
     unemployed: true,
     designations: [
       `Coding Monkey`,
